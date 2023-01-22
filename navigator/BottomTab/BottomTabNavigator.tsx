cbfccba69f4c57ffec95aa18bf7ae285343ac5cf
minhnhatle104 <lnminh104@gmail.com>
2023-01-22 22:23:02 +0700

    Fixing bottom tab navigation

--- navigator/BottomTab/BottomTabNavigator.tsx
+++ navigator/BottomTab/BottomTabNavigator.tsx
@@ -1,6 +1,6 @@
 import React,{MutableRefObject, useEffect,useRef} from "react"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import * as Animatable from 'react-native-animatable';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -31,39 +31,53 @@ const TabArr:TabScreen[] = [
 
 const BottomTabStack = createBottomTabNavigator<TabStackParamList>();
 
+const animate1 = { 0: { scale: .5, translateY: 7 }, .92: { translateY: -34 }, 1: { scale: 1.2, translateY: -24 } }
+const animate2 = { 0: { scale: 1.2, translateY: -24 }, 1: { scale: 1, translateY: 7 } }
+
+const circle1 = { 0: { scale: 0 }, 0.3: { scale: .9 }, 0.5: { scale: .2 }, 0.8: { scale: .7 }, 1: { scale: 1 } }
+const circle2 = { 0: { scale: 1 }, 1: { scale: 0 } }
+
 const TabButton = (props:any) => {
     const { item, onPress, accessibilityState } = props;
     const focused = accessibilityState.selected;
     const viewRef = React.useRef<any>(null);
+    const circleRef = React.useRef<any>(null);
     const textRef =  React.useRef<any>(null);
   
     useEffect(() => {
       if (focused) {
-        viewRef.current.animate({0: {scale: 1}, 1: {scale: 1.5}});
-        textRef.current.transitionTo({ scale: 1.5 });
+        viewRef.current.animate(animate1);
+        circleRef.current.animate(circle1);
+        textRef.current.transitionTo({ scale: 1 });
       } else {
-        viewRef.current.animate({0: {scale: 1.5}, 1: {scale: 1}});
+        viewRef.current.animate(animate2);
+        circleRef.current.animate(circle2);
         textRef.current.transitionTo({ scale: 0 });
       }
     }, [focused])
   
     return (
       <TouchableOpacity
-        onPress={onPress}
-        activeOpacity={1}
+      onPress={onPress}
+      activeOpacity={1}
+      style={styles.container}>
+      <Animatable.View
+        ref={viewRef}
+        duration={1000}
         style={styles.container}>
-        <Animatable.View
-          ref={viewRef}
-          duration={1000}
-          style={styles.container}>
-          <Icon name={item.activeIcon} size={CONSTANST.iconSize} color={focused ? Colors.red : Colors.primaryLite} />
-        </Animatable.View>
+        <View style={styles.btn}>
+          <Animatable.View
+            ref={circleRef}
+            style={styles.circle} />
+          <Icon name={item.activeIcon} size={CONSTANST.iconSize} color={focused ? Colors.white : Colors.gray} />
+        </View>
         <Animatable.Text
           ref={textRef}
           style={styles.text}>
           {item.label}
         </Animatable.Text>
-      </TouchableOpacity>
+      </Animatable.View>
+    </TouchableOpacity>
     )
   }
 
@@ -91,15 +105,62 @@ export const BottomTabNavigator = () => {
 }
 
 
+export default function AnimTab1() {
+  return (
+    <BottomTabStack.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: styles.tabBar,
+      }}
+    >
+      {TabArr.map((item, index) => {
+        return (
+          <BottomTabStack.Screen key={index} name={item.route} component={item.component}
+            options={{
+              tabBarShowLabel: false,
+              tabBarButton: (props) => <TabButton {...props} item={item} />
+            }}
+          />
+        )
+      })}
+    </BottomTabStack.Navigator>
+  )
+}
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    text: {
-        fontSize: 10,
-        textAlign: 'center',
-        color: Colors.red,
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tabBar: {
+    height: 70,
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+    left: 16,
+    borderRadius: 16,
+  },
+  btn: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 4,
+    borderColor: Colors.white,
+    backgroundColor: Colors.white,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  circle: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.red,
+    borderRadius: 25,
+  },
+  text: {
+    fontSize: 10,
+    textAlign: 'center',
+    color: Colors.red,
+  }
 })
