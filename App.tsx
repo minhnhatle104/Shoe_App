9db1bac4cdea76d67035204471207c6d8d7c7a12
minhnhatle104 <lnminh104@gmail.com>
2023-01-22 15:27:53 +0700

    Splash Screen finish

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import 'react-native-gesture-handler';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { DrawerNavigator, StackNavigator } from './navigator/Navigator';
+import SplashScreen from 'react-native-splash-screen'
 
 
 type Props = {}
 
 const App = (props: Props) => {
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[])
   return (
     <NavigationContainer>
         <StackNavigator/>
